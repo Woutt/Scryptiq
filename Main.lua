a97e38af40bb928db101e1eef33edd5bbdcf80d3
Sghaap_ <71903277+Woutt@users.noreply.github.com>
2026-01-09 22:32:59 +0100

    Update Main.lua
    Load external scripts for file system and game handling.

--- Main.lua
+++ Main.lua
@@ -1,1 +1,13 @@
---test
+--//File system
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Woutt/Scryptiq/main/Libex/File_system.lua"))() 
+FS_func.FileSystem("Scryptiq/GameSaves/")
+FS_func.FileSystem("Scryptiq/Assets/")
+
+--//Load api and libex
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Woutt/Scryptiq/main/Misc/API.lua"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Woutt/Scryptiq/main/Libex/Libary"))()
+
+--//Find game
+local kick,String = function() game:GetService("Players").LocalPlayer:Kick("Scryptiq doesnt support this game, discord link copied to clipboard!") end
+local Error = pcall(function() String = game:HttpGet("https://raw.githubusercontent.com/Woutt/Scryptiq/main/Games/"..game.PlaceId) end)
+if Error == false then Invite() else loadstring(String)() end
